7791b43eb49d314d45e51861f62213dcd29cccfc
tyl0r3 <tylerm874@gmail.com>
2016-09-24 13:53:45 -0400

    Just updating my business

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,14 @@
+#TEST
+
 #Default is "World" 
 #Author: Tyler Miller (tjmille2@oakland.edu)
 puts "Hello, #{ARGV.first}!"
 puts "Hello, #{name}!"
+
+require 'greeter'
+
+# Default is World
+name = ARGV.first || "World"
+
+greeter = Greeter.new(name)
+puts greeter.greet
